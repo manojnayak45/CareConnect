afc907e9f75d4999d97e67ef656ba4a5dac406a2
Rathod Manoj Kumar <rathodmanoj2022@gmail.com>
2024-10-06 23:15:13 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,53 @@
+import express from "express";
+import { dbConnection } from "./database/dbConnection.js";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import fileUpload from "express-fileupload";
+import { errorMiddleware } from "./middlewares/error.js";
+import messageRouter from "./router/messageRouter.js";
+import userRouter from "./router/userRouter.js";
+import appointmentRouter from "./router/appointmentRouter.js";
+
+const app = express();
+
+//config({ path: "./.env" });
+config();
+
+const allowedOrigins = ["http://localhost:5173"];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ["GET", "POST", "DELETE", "PUT"],
+  credentials: true
+};
+
+// Enable CORS with the specified options
+app.use(cors(corsOptions));
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+app.use("/api/v1/message", messageRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/appointment", appointmentRouter);
+
+dbConnection();
+
+app.use(errorMiddleware);
+export default app;
